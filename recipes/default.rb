b849f88f02761da585cecd3edc942a36372bf59d
Eric Hanko <v-erhank@microsoft.com>
2018-03-23 23:32:11 -0700

    add attributes to compare designs

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+node['macos']['image_capture']['hot_plug'] = true
+node['macos']['software_updates']['download'] = false
+node['macos']['software_updates']['check_for_updates'] = false
+
+node['macos']['bluetooth']['keyboard_missing'] = false
+node['macos']['bluetooth']['mouse_missing'] = false
+
+node['macos']['new_to_mac'] = false
+node['macos']['remote_management'] = true
+node['macos']['time_machine']['offer_new_disks'] = false
+node['macos']['screensaver']['idle_time'] = false
